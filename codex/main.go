00766185cbc753dc2046597d4096e0086861fe32
ByK95 <byrmkaya97@gmail.com>
2025-08-11 23:19:10 +0300

    update goitems

--- codex/main.go
+++ codex/main.go
@@ -382,7 +382,7 @@ func RollLoot(itemsPtr *C.LootRow, length C.int) *C.Loot {
 	goItems := make([]loot.LootRow, len(cItems))
 	for i, item := range cItems{
 		goItems[i] = loot.LootRow{
-            Id:    int32(item.id),
+            ID:    int32(item.id),
             Chance: float32(item.chance),
             Pity:  int32(item.pity),
         }
