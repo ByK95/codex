03a2f6294bd0582137e0774a0f7196089b6359c4
ByK95 <byrmkaya97@gmail.com>
2025-09-11 17:58:53 +0300

    fix struct name typo

--- codex/main.go
+++ codex/main.go
@@ -531,7 +531,7 @@ func Voronoi_ZoneAt(x C.int, y C.int) C.int {
 }
 
 //export Voronoi_GetRandomInRadius
-func Voronoi_GetRandomInRadius(x C.int, y C.int, zoneId C.int, radius C.int) C.Coord {
+func Voronoi_GetRandomInRadius(x C.int, y C.int, zoneId C.int, radius C.int) C.Coord2d {
     px, py, ok := voronoi.RandomPositionInRadius(int(x), int(y), int(zoneId), int(radius));
 	if !ok {
 		return C.MakeCoord(C.int(-1), C.int(-1))
